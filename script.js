49eae01761fa77294c5f8e11e662e07eb032a8b7
aryantorfehneja <aryan.torfeh-nejad@ocsbstudent.ca>
2021-03-17 14:38:18 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('button').addEventListener
+('click', displayMessage)
+
+function displayMessage () {
+  document.getElementById('heading').innerHTML="Why is this not working?"
+}
